b6143ac58a09c8e986d0c0ecab130a2938a8a34b
bazzl1202 <71136617+bazzl1202@users.noreply.github.com>
2020-10-05 11:09:58 -0400

    Asks user for positive integer and tells them factors of it

--- Main.java
+++ Main.java
@@ -15,15 +15,34 @@ public class Main {
 
     // declare integer variable
     int posInt;
-
+ 
+    // only allows user to enter positive integer
     do{
-      // ask the user for a postivie integer
-    System.out.println("Please enter a positive integer to determine its factors:");
-    posInt = input.nextInt();
+     // ask the user for a postivie integer
+     System.out.println("Please enter a positive integer to determine its factors:");
+     posInt = input.nextInt();
     } while(posInt <= 0);
 
-  
+   // declare and initilaize counting variable
+    int count = 1;
 
-    
+    // declare factor variable
+    int factor;
+
+    // tell the user what the factors
+    System.out.println("The factors of " + posInt + " are:");
+
+    // goes through all values up to integer to check for factors
+    while(count <= posInt){
+      if( posInt%count == 0){
+        // division to solve for factors
+        factor = posInt/count;
+        // prints to the user the factors
+        System.out.println(factor);
+      }
+      // moves up one number and loop repeats unless greater than posInt
+      count = count + 1;
+    }
+
   }
 }
